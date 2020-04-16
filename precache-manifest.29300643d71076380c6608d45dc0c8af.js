53c1ee6297b0593046770dd133859af2c065b7a4
lnguyen9621 <lnguyen9621@gmail.com>
2020-04-15 19:06:11 -0600

    Updates

---
+++ precache-manifest.29300643d71076380c6608d45dc0c8af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f244bf7cf9f0551c1391901874a372b",
+    "url": "/nhipsongdongbang/index.html"
+  },
+  {
+    "revision": "af1bad23910a0387ba60",
+    "url": "/nhipsongdongbang/static/css/main.2ef0b9cc.chunk.css"
+  },
+  {
+    "revision": "aa273b1c19de7aa9523d",
+    "url": "/nhipsongdongbang/static/js/2.ed3e41e3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/nhipsongdongbang/static/js/2.ed3e41e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af1bad23910a0387ba60",
+    "url": "/nhipsongdongbang/static/js/main.907ef139.chunk.js"
+  },
+  {
+    "revision": "d08db0331c01006bdb13",
+    "url": "/nhipsongdongbang/static/js/runtime-main.3061dc1c.js"
+  }
+]);
